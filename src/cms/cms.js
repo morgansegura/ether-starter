1b629a264a2bdb0c4e53364b3d0bd3746a7dad9f
Morgan Segura <morgansegura@gmail.com>
2018-11-15 23:17:59 -0800

    Update CMS files

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,8 +2,8 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+import PricingPagePreview from './preview-templates/PricingPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('pricing', PricingPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
