5c8a6826153edcdf304d970de41a4e4b7ea61c24
Morgan Segura <morgansegura@gmail.com>
2018-11-29 14:18:31 -0800

    Test video player

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,26 @@ module.exports = {
           },
         ],
       },
-    },     
+    }, 
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-better-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77.
+              height: 400, // Optional: Overrides optional.ratio.
+              related: false, // Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, // Optional: Disable insertion of <style> border: 0.
+              showInfo: false // Optional: Hides video title and player actions.
+            }
+          },
+          `gatsby-remark-responsive-iframe`
+        ]
+      }
+    },      
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
@@ -77,7 +96,7 @@ module.exports = {
         htmlTitle: `Content Manager`,
       },
     },
-    'gatsby-plugin-stripe-checkout',
+    // 'gatsby-plugin-stripe-checkout',
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
