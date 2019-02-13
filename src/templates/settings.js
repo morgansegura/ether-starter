b1327c3b5ce983720e6a9b4896462dbdb2c6d921
Morgan Segura <morgansegura@gmail.com>
2019-02-13 15:18:56 -0800

    Add setting to admin pane;

--- src/templates/settings.js
+++ src/templates/settings.js
@@ -3,84 +3,85 @@ import PropTypes from 'prop-types'
 import { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import Content, { HTMLContent } from '../components/Content'
 
 export const SettingsTemplate = ({
-    content,
-    contentComponent,
-    description,
-    tags,
-    title,
-    helmet,
+  content,
+  contentComponent,
+  description,
+  tags,
+  title,
+  helmet,
 }) => {
-    const SettingsContent = contentComponent || Content
+  const SettingsContent = contentComponent || Content
 
-    return (
-        <section className="section">
-            {helmet || ''}
-            <div className="container content">
-                <div className="columns">
-                    <div className="column is-10 is-offset-1">
-                        <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-                            {title}
-                        </h1>
-                        <p>{description}</p>
-                        <SettingsContent content={content} />
-                        {tags && tags.length ? (
-                            <div style={{ marginTop: `4rem` }}>
-                                <h4>Tags</h4>
-                                <ul className="taglist">
-                                    {tags.map(tag => (
-                                        <li key={tag + `tag`}>
-                                            <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        ) : null}
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="section">
+      {helmet || ''}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <p>{description}</p>
+            <SettingsContent content={content} />
+            {tags && tags.length ? (
+              <div style={{ marginTop: `4rem` }}>
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map(tag => (
+                    <li key={tag + `tag`}>
+                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 SettingsTemplate.propTypes = {
-    content: PropTypes.node.isRequired,
-    contentComponent: PropTypes.func,
-    description: PropTypes.string,
-    title: PropTypes.string,
-    helmet: PropTypes.object,
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
 }
 
 const Settings = ({ data }) => {
-    const { markdownRemark: post } = data
+  const { markdownRemark: post } = data
 
-    return (
-        <Layout>
-            <SettingsTemplate
-                content={post.html}
-                contentComponent={HTMLContent}
-                description={post.frontmatter.description}
-                helmet={
-                    <Helmet
-                        titleTemplate="%s | Settings"
-                    >
-                        <title>{`${post.frontmatter.title}`}</title>
-                        <meta name="description" content={`${post.frontmatter.description}`} />
-                    </Helmet>
-                }
-                title={post.frontmatter.title}
+  return (
+    <Layout>
+      <SettingsTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        description={post.frontmatter.description}
+        helmet={
+          <Helmet titleTemplate="%s | Settings">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post.frontmatter.description}`}
             />
-        </Layout>
-    )
+          </Helmet>
+        }
+        title={post.frontmatter.title}
+      />
+    </Layout>
+  )
 }
 
 Settings.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.object,
-    }),
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
 }
 
 export default Settings
