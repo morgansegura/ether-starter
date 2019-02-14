5f2c68f01ea80390d25493e2d1d2f516427c9de2
Morgan Segura <morgansegura@gmail.com>
2019-02-13 22:02:18 -0800

    Update settings in admin

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,9 +4,11 @@ import * as ColorWidget from 'netlify-cms-widget-color'
 import HomePagePreview from './preview-templates/HomePagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import CustomPagePreview from './preview-templates/CustomPagePreview'
+import SettingsPagePreview from './preview-templates/SettingsPagePreview'
 
 CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('post', BlogPostPreview)
 CMS.registerPreviewTemplate('page', CustomPagePreview)
+CMS.registerPreviewTemplate('settings', SettingsPagePreview)
 
 CMS.registerWidget('color', ColorWidget.Control)
---
+++ src/pages/settings/menus.md
@@ -0,0 +1,13 @@
+---
+templateKey: settings
+menuHeader:
+  menuItems:
+    - label: Home
+      linkTitle: Home
+      linkType: internal
+      linkURL: /
+    - label: About
+      linkTitle: About Us
+      linkType: internal
+      linkURL: /about
+---
